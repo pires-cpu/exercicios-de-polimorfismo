ad8cce5ff1e9e6342fe91df4a53ec8167e0e9895
Aluno São Lucas <aluno.saolucas@ensino.ulbra.net>
2026-03-26 15:20:08 -0300

    atividade 4

--- atividade 3/src/atividade/pkg3/Violao.java
+++ atividade 3/src/atividade/pkg3/Violao.java
@@ -9,6 +9,45 @@ package atividade.pkg3;
  *
  * @author aluno.saolucas
  */
-public class Violao {
-    
+public class Violao extends Instrumento {
+
+    private int cordas;
+    private String madeira;
+    private boolean eletrico;
+
+    public Violao(String nome, String tipo, String fabricante, int cordas, String madeira, boolean eletrico) {
+        super(nome, tipo, fabricante);
+        this.cordas = cordas;
+        this.madeira = madeira;
+        this.eletrico = eletrico;
+    }
+
+    public int getCordas() {
+        return cordas;
+    }
+
+    public void setCordas(int cordas) {
+        this.cordas = cordas;
+    }
+
+    public String getMadeira() {
+        return madeira;
+    }
+
+    public void setMadeira(String madeira) {
+        this.madeira = madeira;
+    }
+
+    public boolean isEletrico() {
+        return eletrico;
+    }
+
+    public void setEletrico(boolean eletrico) {
+        this.eletrico = eletrico;
+    }
+
+    @Override
+    public void tocar() {
+        System.out.println("O violão está tocando");
+    }
 }
